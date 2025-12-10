d28c57392ef15c88bc21b67e2ccff0d5866f34c0
Cristian Nastase <cristian.nastae94@protonmail.com>
2025-12-10 12:45:12 +0200

    Gasirea ip-ului a fost modificata daca nu se afla in /etc/hosts

--- nslookup.sh
+++ nslookup.sh
@@ -3,14 +3,12 @@
 if [ -z "$3" ];then
 	echo "Nu s-a transmis serverul DNS, va fi omis"
 	look=`nslookup $1`
-	last_word=$(echo "$look" | awk '{print $NF}' | tail -n 3 )
 else
-	look=`nslookup $1 $3`
-	last_word=$(echo "$look" | awk '{print $NF}' | tail -n 3 | head -n 1)
+	look=`nslookup $1 $3`	
 fi
 
-echo "$last_word" 
-echo "$2"
+last_word=$(echo "$look" | awk '{print $NF}' | tail -n 3 | head -n 1)
+
 if [ "$last_word" != "$2" ]; then
 	echo "Bogus IP for $1"
 else
